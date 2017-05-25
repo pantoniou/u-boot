kaustav-net/ti-uboot

drivers: mmc: omap_hsmmc: allow mmc clock to be gated
mmc core has defined a new parameter *clk_disable*
to gate the clock. Disable the clock here if
*clk_disable* is set.

Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>

drivers/mmc/omap_hsmmc.c
@@ -1079,12 +1079,18 @@ static void omap_hsmmc_set_bus_width(struct mmc *mmc)
 static int omap_hsmmc_set_ios(struct mmc *mmc)
 {
 	struct omap_hsmmc_data *priv_data = mmc->priv;
+	struct hsmmc *mmc_base = priv_data->base_addr;
 
 	if (priv_data->bus_width != mmc->bus_width)
 		omap_hsmmc_set_bus_width(mmc);
 
 	if (priv_data->clock != mmc->clock)
 		omap_hsmmc_set_clock(mmc);
+
+	if (mmc->clk_disable)
+		omap_hsmmc_stop_clock(mmc_base);
+	else
+		omap_hsmmc_start_clock(mmc_base);
 
 #ifdef CONFIG_DM_MMC
 #ifdef CONFIG_IODELAY_RECALIBRATION

pantoniou/u-boot

drivers/mmc/omap_hsmmc.c
@@ -1139,11 +1139,18 @@ static int omap_hsmmc_set_ios(struct udevice *dev)
 	struct mmc_uclass_priv *upriv = dev_get_uclass_priv(dev);
 	struct mmc *mmc = upriv->mmc;
 #endif
+	struct hsmmc *mmc_base = priv->base_addr;
+
 	if (priv->bus_width != mmc->bus_width)
 		omap_hsmmc_set_bus_width(mmc);
 
 	if (priv_data->clock != mmc->clock)
 		omap_hsmmc_set_clock(mmc);
+
+	if (mmc->clk_disable)
+		omap_hsmmc_stop_clock(mmc_base);
+	else
+		omap_hsmmc_start_clock(mmc_base);
 
 #ifdef CONFIG_DM_MMC
 #ifdef CONFIG_IODELAY_RECALIBRATION